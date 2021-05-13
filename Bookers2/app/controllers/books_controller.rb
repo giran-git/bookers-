088ad32aea4ec3b243f1edc8992b0aa0aaa94bf6
giran-git <giran904587034+git@gmail.com>
2021-05-13 10:48:51 +0000

    first commit

---
+++ Bookers2/app/controllers/books_controller.rb
@@ -0,0 +1,21 @@
+class BooksController < ApplicationController
+  def show
+    @book = Book.new
+  end
+
+  def index
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
--- Bookers2/config/routes.rb
+++ Bookers2/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   root to: 'homes#top'
+  resources :user, only:[:show, :index, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

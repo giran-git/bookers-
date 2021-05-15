d47630637b6009d56d90c0cd2f484ece6ec19692
giran-git <giran904587034+git@gmail.com>
2021-05-15 08:57:10 +0000

    first commit

--- bookers2/config/routes.rb
+++ bookers2/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'users/show'
   devise_for :users
   root to:'homes#top'
+  get 'homes/about' => 'homes#about'
  resources :books, only: [:index, :show, :create, :edit, :update, :destroy]
 
 end

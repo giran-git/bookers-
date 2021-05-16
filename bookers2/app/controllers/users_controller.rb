7ba90edf52135043348926e32661d6d06a6c1d14
giran-git <giran904587034+git@gmail.com>
2021-05-16 13:56:31 +0000

    削除

--- bookers2/app/controllers/users_controller.rb
+++
@@ -1,6 +0,0 @@
-class UsersController < ApplicationController
-  def show
-    @user = User.find(params[:id])
-  end
-  resources :users, only: [:index, :show, :edit, :update]
-end
--- bookers2/config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  get 'users/show'
-  devise_for :users
-  root to:'homes#top'
-  get 'homes/about' => 'homes#about'
- resources :books, only: [:index, :show, :create, :edit, :update, :destroy]
-
-end
